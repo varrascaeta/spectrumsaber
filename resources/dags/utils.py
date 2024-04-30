32163bbe7c59937edb56db79395cfef40018ef7f
varrascaeta <varrascaeta@seenka.com>
2024-04-30 01:22:31 -0300

    get ftp files parsed

--- resources/dags/utils.py
+++ resources/dags/utils.py
@@ -1,9 +1,18 @@
 # Standard imports
 import logging
 from ftplib import FTP
+import re
+import os
+from datetime import datetime
 
 
 logger = logging.getLogger(__name__)
+DIR_LIST_PATTERN = (
+    r'^(\d{2}-\d{2}-\d{2})\s+(\d{2}:\d{2}(?:AM|PM))\s+'
+    r'(<DIR>|\d+)\s+(.+)$'
+)
+DATE_FORMAT = "%m-%d-%y"
+TIME_FORMAT = "%I:%M%p"
 
 
 class FTPClient():
@@ -14,19 +23,36 @@ class FTPClient():
         self.connection = None
 
     def connect(self) -> FTP:
-        if not self.connection:
-            logger.info("Connecting to %s", self.host)
-            self.connection = FTP(self.host, encoding="latin-1")
-            status = self.connection.login(self.username, self.password)
-            logger.info("Status: %s", status)
-        else:
-            logger.info("Already connected to %s", self.host)
+        logger.info("Connecting to %s", self.host)
+        self.connection = FTP(self.host, encoding="latin-1")
+        status = self.connection.login(self.username, self.password)
+        logger.info("Status: %s", status)
 
-    def get_files(self, path: str) -> list['str']:
+    def get_files(self, path: str) -> list['dict']:
         self.connect()
         logger.info("Scanning %s", path)
-        files = self.connection.nlst(path)
-        return files
+        self.connection.cwd(path)
+        lines = []
+        self.connection.dir(lines.append)
+        self.connection.quit()
+        parsed_files = [self.parse_line(path, line) for line in lines]
+        return parsed_files
+
+    def parse_line(self, path, line):
+        match = re.match(DIR_LIST_PATTERN, line.strip())
+        if match:
+            date_str, time_str, kind, filename = match.groups()
+            date = datetime.strptime(date_str, DATE_FORMAT).date()
+            time = datetime.strptime(time_str, TIME_FORMAT).time()
+            created_at = datetime.combine(date, time)
+            return {
+                "filepath": os.path.join(path, filename),
+                "created_at": created_at,
+                "is_dir": kind == "<DIR>",
+            }
+        else:
+            logger.error(f"Line {line} does not match pattern")
+            return {}
 
     def __str__(self) -> str:
         return f"FTP:{self.username}@{self.host}"
