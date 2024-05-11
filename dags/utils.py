d6b09d490aafdc82d91af3d2e8e3d6b708c58572
varrascaeta <varrascaeta@seenka.com>
2024-05-11 20:35:27 -0300

    use try if folder has no access

--- dags/utils.py
+++ dags/utils.py
@@ -1,9 +1,13 @@
 # Standard imports
+import functools
 import logging
-from ftplib import FTP
-import re
 import os
+import re
 from datetime import datetime, UTC
+# Extra imports
+import inspect
+import pickle
+from ftplib import FTP
 
 
 logger = logging.getLogger(__name__)
@@ -31,23 +35,20 @@ class FTPClient():
         logger.info("Status: %s", status)
 
     def get_dir_data(self, path: str) -> list[dict]:
-        retries = 0
-        while retries < 3:
-            try:
-                logger.info("Scanning %s", path)
-                self.connection.cwd(path)
-                lines = []
-                self.connection.dir(lines.append)
-                parsed_files = []
-                for line in lines:
-                    parsed = self.parse_line(path, line)
-                    if parsed:
-                        parsed_files.append(parsed)
-                return parsed_files
-            except Exception as e:
-                logger.error("Error scanning %s: %s", path, e)
-                self.connect()
-                retries += 1
+        try:
+            logger.info("Scanning %s", path)
+            self.connection.cwd(path)
+            lines = []
+            self.connection.dir(lines.append)
+            parsed_files = []
+            for line in lines:
+                parsed = self.parse_line(path, line)
+                if parsed:
+                    parsed_files.append(parsed)
+            return parsed_files
+        except Exception as e:
+            logger.error(f"Error scanning {path}: {e}")
+            return []
 
     def parse_line(self, path, line):
         match = re.match(DIR_LIST_PATTERN, line.strip())
@@ -68,3 +69,38 @@ class FTPClient():
 
     def __str__(self) -> str:
         return f"FTP:{self.username}@{self.host}"
+
+
+def serialize(output=True, inputs=True):
+    def _wrapper(func):
+        parse_inputs = inputs
+        if inputs is True:
+            parse_inputs = list(inspect.signature(func).parameters)
+        elif inputs is False:
+            parse_inputs = []
+
+        @functools.wraps(func)
+        def _dec(**kwargs):
+            final_kwargs = {}
+            for k, v in kwargs.items():
+                if k in parse_inputs:
+                    v = pickle.loads(v)
+                final_kwargs[k] = v
+            result = func(**final_kwargs)
+            result = pickle.dumps(result) if output else result
+            return result
+        return _dec
+    return _wrapper
+
+
+def serialize_model(serializer):
+    def _wrapper(func):
+        @functools.wraps(func)
+        def _dec(**kwargs):
+            result = func(**kwargs)
+            if result:
+                serialized = serializer(result).data
+                return serialized
+            return result
+        return _dec
+    return _wrapper
