9c1e5c2cce0ab11a60e6a069430f3e8f1599db15
varrascaeta <varrascaeta@seenka.com>
2024-06-10 19:00:14 -0300

    Delete city and create province instead

--- resources/places/models.py
+++ resources/places/models.py
@@ -18,12 +18,17 @@ class Country(models.Model):
     class Meta:
         verbose_name_plural = "Countries"
 
-class City(models.Model):
+
+class Province(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=8, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name="provinces"
+    )
 
     def __str__(self) -> str:
         return f"{self.name} ({self.code})" if self.code else self.name
@@ -31,10 +36,21 @@ class City(models.Model):
 
 class District(models.Model):
     name = models.CharField(max_length=255)
-    code = models.CharField(max_length=8, null=True)
+    code = models.CharField(max_length=16, null=True, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    province = models.ForeignKey(
+        Province,
+        on_delete=models.CASCADE,
+        related_name="districts"
+    )
 
     def __str__(self) -> str:
         return f"{self.name} ({self.code})" if self.code else self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "province"],
+                name="unique_district"
+            )
+        ]
