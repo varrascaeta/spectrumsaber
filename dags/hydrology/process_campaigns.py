cfd9ff8311687f2c2f0d2bff8554889d782869cb
varrascaeta <varrascaeta@seenka.com>
2024-10-01 23:08:59 -0300

    Create hydro campaigns with operators

--- dags/hydrology/process_campaigns.py
+++ dags/hydrology/process_campaigns.py
@@ -2,17 +2,13 @@
 import logging
 from datetime import datetime
 # Airflow imports
-from airflow.decorators import dag, task
-# Django imports
-from django.utils import timezone
+from airflow.decorators import dag
 # Project imports
-from dags.operators import DjangoOperator
+from dags.operators import ProcessObjectsOperator, GetObjectDataOperator
 
 
 # Globals
 logger = logging.getLogger(__name__)
-current_task = 0
-total_tasks = 0
 
 
 @dag(
@@ -23,41 +19,19 @@ total_tasks = 0
     tags=["campaigns", "hydro"],
 )
 def process_hydro_campaigns() -> None:
-    setup_django = DjangoOperator(task_id="setup_django")
+    coverage_data = GetObjectDataOperator(
+        task_id="get_coverage_data",
+        model_path="resources.campaigns.models.Coverage",
+        obj_name="HIDROLOGIA",
+    )
 
-    @task()
-    def init_unmatched_file() -> None:
-        run_date = timezone.now().strftime("%Y-%m-%d %H:%M:%s")
-        with open("unmatched_campaigns_hydro.txt", "a") as f:
-            f.write("="*80)
-            f.write(f"\nUnmatched campaigns on {run_date}\n")
+    campaigns = ProcessObjectsOperator(
+        task_id="create_campaigns",
+        creator_module="resources.campaigns.campaign_creators.CampaignCreator",
+        parent_data=coverage_data.output,
+    )
 
-    @task()
-    def get_coverage_data() -> dict:
-        from resources.campaigns.models import Coverage
-        try:
-            coverage = Coverage.objects.get(name="HIDROLOGIA")
-            return {"id": coverage.id, "path": coverage.path}
-        except Coverage.DoesNotExist:
-            logger.info(
-                "Coverage not found. Try running process_coverage dag first..."
-            )
-
-    @task()
-    def create_campaigns(coverage_data: dict) -> None:
-        from resources.campaigns.campaign_creators import process_objects
-        process_objects(
-            coverage_tag="hydro",
-            creator_key="campaigns",
-            parent_ids=[coverage_data["id"]],
-        )
-
-    # Define flow
-    coverage_data = get_coverage_data()
-    init_file = init_unmatched_file()
-    campaigns = create_campaigns(coverage_data=coverage_data)
-
-    setup_django >> coverage_data >> init_file >> campaigns
+    coverage_data >> campaigns
 
 
 dag = process_hydro_campaigns()
