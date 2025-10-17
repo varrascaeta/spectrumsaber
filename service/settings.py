29410f374584617eaaeae963b31dcc975bfc68c6
varrascaeta <valentin.arrascaeta@muttdata.ai>
2025-10-17 10:58:59 -0300

    fix: separate postgres env vars

--- service/settings.py
+++ service/settings.py
@@ -113,8 +113,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'USER': os.getenv('APP_POSTGRES_USER'),
+        'PASSWORD': os.getenv('APP_POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT'),
     }
