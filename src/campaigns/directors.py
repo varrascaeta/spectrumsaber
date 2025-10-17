9ea4555e23cb5e67fd25b93a38d73b259d92bba8
varrascaeta <valentin.arrascaeta@muttdata.ai>
2025-10-17 19:27:57 -0300

    fix: merge common function calls into superclass

--- src/campaigns/directors.py
+++ src/campaigns/directors.py
@@ -1,5 +1,11 @@
 import abc
-from src.campaigns.builders import BaseBuilder, CoverageBuilder, CampaignBuilder, DataPointBuilder
+from src.campaigns.builders import (
+    BaseBuilder,
+    CoverageBuilder,
+    CampaignBuilder,
+    DataPointBuilder,
+    UnmatchedBuilder
+)
 from src.campaigns.models import BaseFile
 from src.logging_cfg import setup_logger
 logger = setup_logger(__name__)
@@ -12,9 +18,16 @@ class BaseDirector(abc.ABC):
     def _get_builder(self) -> BaseBuilder:
         pass
 
-    @abc.abstractmethod
     def construct(self, file_data: dict) -> BaseFile:
-        pass
+        # Base File attributes
+        self._builder.build_instance(path=file_data["path"])
+        self._builder.build_name(file_data["name"])
+        self._builder.build_metadata(file_data.get("metadata", {}))
+        self._builder.build_description(file_data.get("description", ""))
+        self._builder.build_ftp_created_at(file_data["created_at"])
+        self._builder.build_is_unmatched()
+        self._builder.build_last_synced_at()
+        self._builder.build_parent(parent_path=file_data.get("parent", ""))
 
     def commit(self) -> BaseFile:
         self._builder.save_to_db()
@@ -26,24 +39,10 @@ class UnmatchedDirector(BaseDirector):
         return UnmatchedBuilder()
 
     def construct(self, file_data: dict):
-        # Base File attributes
-        self._builder.build_instance(path=file_data["path"])
-        self._builder.build_name(file_data["name"])
-        self._builder.build_description(file_data.get("description", ""))
-        self._builder.build_ftp_created_at(file_data["created_at"])
-        self._builder.build_is_unmatched(True)
-        self._builder.build_last_synced_at()
-        self._builder.build_attributes()
-        self._builder.build_metadata()
-
-        # Campaign attributes
-        self._builder.build_parent(parent_path=file_data.get("parent", ""))
-        self._builder.build_date()
-        self._builder.build_district()
-        self._builder.build_external_id()
-        self._builder.build_measuring_tool()
-        self._builder.build_spreadsheets()
-        logger.info(f"Built Unmatched Campaign: {self._builder.instance.__dict__}")
+        super().construct(file_data)
+        # UnmatchedFile attributes
+        self._builder.build_level(file_data.get("level", ""))
+        logger.info("Built UnmatchedFile: %s", self._builder.instance.__dict__)
 
 
 class CoverageDirector(BaseDirector):
@@ -51,16 +50,7 @@ class CoverageDirector(BaseDirector):
         return CoverageBuilder()
 
     def construct(self, file_data: dict):
-        # Base File attributes
-        self._builder.build_instance(path=file_data["path"])
-        self._builder.build_name(file_data["name"])
-        self._builder.build_description(file_data.get("description", ""))
-        self._builder.build_ftp_created_at(file_data["created_at"])
-        self._builder.build_is_unmatched(file_data.get("is_unmatched", False))
-        self._builder.build_last_synced_at()
-        self._builder.build_attributes()
-        self._builder.build_metadata()
-
+        super().construct(file_data)
         # Coverage attributes
         self._builder.build_parent(parent_path="")  # Coverage has no parent
 
@@ -70,27 +60,14 @@ class CampaignDirector(BaseDirector):
         return CampaignBuilder()
 
     def construct(self, file_data: dict):
-        # Base File attributes
-        self._builder.build_instance(path=file_data["path"])
-        self._builder.build_name(file_data["name"])
-        self._builder.build_attributes()
-        # Check if file matched
-        if not self._builder.attributes:
-
-        self._builder.build_description(file_data.get("description", ""))
-        self._builder.build_ftp_created_at(file_data["created_at"])
-        self._builder.build_is_unmatched(file_data.get("is_unmatched", False))
-        self._builder.build_last_synced_at()
-        self._builder.build_metadata()
-
+        super().construct(file_data)
         # Campaign attributes
-        self._builder.build_parent(parent_path=file_data["parent"])
-        self._builder.build_date()
+        self._builder.build_date(file_data.get("rule_id", ""), file_data.get("date", ""))
+        self._builder.build_external_id(file_data.get("external_id", ""))
         self._builder.build_district()
-        self._builder.build_external_id()
         self._builder.build_measuring_tool()
         self._builder.build_spreadsheets()
-        logger.info(f"Built Campaign: {self._builder.instance.__dict__}")
+        logger.info("Built Campaign: %s", self._builder.instance.__dict__)
 
 
 class DataPointDirector(BaseDirector):
@@ -98,20 +75,10 @@ class DataPointDirector(BaseDirector):
         return DataPointBuilder()
 
     def construct(self, file_data: dict) -> BaseFile:
-        self._builder.file_data = file_data
-        # Base File attributes
-        self._builder.build_name(file_data["name"])
-        self._builder.build_path(file_data["path"])
-        self._builder.build_description(file_data.get("description", ""))
-        self._builder.build_ftp_created_at(file_data["created_at"])
-        self._builder.build_is_unmatched(file_data.get("is_unmatched", False))
-        self._builder.build_last_synced_at()
-        self._builder.build_attributes()
-        self._builder.build_metadata()
-        self._builder.build_order()
+        super().construct(file_data)
 
         # DataPoint attributes
-        self._builder.build_parent()
+        self._builder.build_order()
 
         # Save to DB
         self._builder.save_to_db()
