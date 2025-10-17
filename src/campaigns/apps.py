cc0ee30ce4e1b2b409cd1affb92083811df7a33f
varrascaeta <valentin.arrascaeta@muttdata.ai>
2025-10-17 19:26:44 -0300

    feat: add signal to update unmatched files on rule change

--- src/campaigns/apps.py
+++ src/campaigns/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CampaignsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'src.campaigns'
+
+    def ready(self):
+        import src.campaigns.signals  # noqa
