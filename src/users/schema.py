a5bfeb69469fd909e44ceb278eac23de473deb04
varrascaeta <valentin.arrascaeta@muttdata.ai>
2025-10-24 10:54:48 -0300

    refactor: modularize chemas and types

---
+++ src/users/schema.py
@@ -0,0 +1,20 @@
+# Third-party imports
+import strawberry
+from gqlauth.user import arg_mutations as mutations
+
+@strawberry.type
+class AuthMutation():
+    verify_token = mutations.VerifyToken.field
+    update_account = mutations.UpdateAccount.field
+    archive_account = mutations.ArchiveAccount.field
+    delete_account = mutations.DeleteAccount.field
+    password_change = mutations.PasswordChange.field
+    token_auth = mutations.ObtainJSONWebToken.field
+    register = mutations.Register.field
+    verify_account = mutations.VerifyAccount.field
+    resend_activation_email = mutations.ResendActivationEmail.field
+    send_password_reset_email = mutations.SendPasswordResetEmail.field
+    password_reset = mutations.PasswordReset.field
+    password_set = mutations.PasswordSet.field
+    refresh_token = mutations.RefreshToken.field
+    revoke_token = mutations.RevokeToken.field
